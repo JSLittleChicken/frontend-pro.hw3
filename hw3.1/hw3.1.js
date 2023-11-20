d9630c1fc010edab56b4ae4f0b1d18214f892f57
JSLittleChicken <oksana.petrovich.2019@gmail.com>
2023-11-20 21:06:51 +0200

    hw3 finished

---
+++ hw3.1/hw3.1.js
@@ -0,0 +1,5 @@
+let firstLine = prompt("Введіть перше повідомлення");
+let secondLine = prompt("Введіть друге повідомлення");
+let thirdLine = prompt("Введіть третє повідомлення");
+
+alert(firstLine + secondLine + thirdLine);
---
+++ hw3.2/hw3.2.js
@@ -0,0 +1,3 @@
+let x = prompt("Введіть п'ятизначне число");
+let word = String(x);
+alert(word[0] + " " + word[1] + " " + word[2] + " " + word[3] + " " + word[4]);
